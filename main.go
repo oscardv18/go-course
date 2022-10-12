fb93fef88daa656272283f8755d54ae926ee046c
oscardv18 <oscarddiazvelasquez@gmail.com>
2022-10-11 22:13:17 -0400

    If else statement finish

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
@@ -8,36 +11,55 @@ func main() {
 	var remainingTickets uint8 = 50
 	var bookings []string
 
+	// print welcome messages
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint8
+	// infinite for in golang
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint8
 
-	// ask user for their info
-	fmt.Println("Enter your first name")
-	fmt.Scan(&firstName)
+		// ask user for their info
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter number of tickets")
-	fmt.Scan(&userTickets)
-	bookings = append(bookings, firstName+" "+lastName)
+		fmt.Println("Enter number of tickets")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		if userTickets <= remainingTickets {
+			remainingTickets -= userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Bookings: %v \n", bookings)
-	fmt.Printf("Bookings type %T \n", bookings)
-	fmt.Println("Bookings length ", len(bookings))
+			fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v remining tickets for %v\n", remainingTickets, conferenceName)
 
-	remainingTickets -= userTickets
+			firstNames := []string{}
+			// for each in golang
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
 
-	fmt.Printf("%v remining tickets for %v\n", remainingTickets, conferenceName)
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out, Come Back next year!")
+				break
+			}
+
+		} else {
+			fmt.Printf("We only have %v tickets, so you can't book %v tickets", remainingTickets, userTickets)
+		}
+
+	}
 }
